180161778e6b4192571579b6cfe79087879878f5
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-10-18 16:12:59 +0700

    Update code first
    Update code first

---
+++ Scripts/App.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class App : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+  
+    public void Quit_App()
+    {
+        Application.Quit();
+    }
+}
